03dc988f5ee7aa8c1bc3cf195c6719883ea66111
GordonBreazz <jc12-mail@yandex.ru>
2020-02-12 13:20:58 +0800

    added GoogleMap plugin

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import YmapPlugin from 'vue-yandex-maps'
+import VueGoodTablePlugin from 'vue-good-table'
+import 'vue-good-table/dist/vue-good-table.css'
 
 Vue.use(YmapPlugin)
+Vue.use(VueGoodTablePlugin);
 
 Vue.config.productionTip = false
 
